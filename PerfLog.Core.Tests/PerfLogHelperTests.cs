5e7f4a40059ac0882a80d5717ab5b0f152c27f04
yfliu <yfliu@ctrip.com>
2016-04-07 19:42:16 +0800

    添加单元测试

---
+++ PerfLog.Core.Tests/PerfLogHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PerfLog.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfLog.Core.Tests
+{
+    [TestClass()]
+    public class PerfLogHelperTests
+    {
+        [TestMethod()]
+        public void AttachTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
