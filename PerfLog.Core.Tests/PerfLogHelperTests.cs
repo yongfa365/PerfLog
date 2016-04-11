438a2cabcf49d07bd3c45c15071b677653eb8890
yfliu <yfliu@ctrip.com>
2016-04-11 19:55:34 +0800

    完善测试用例

--- PerfLog.Core.Tests/PerfLogHelperTests.cs
+++ PerfLog.Core.Tests/PerfLogHelperTests.cs
@@ -1,20 +1,216 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PerfLog.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PerfLog.Core.Extend;
 
+/// <summary>
+/// 如果是在这些环境：webform,mvc,webapi，IIS宿主，天生的多线程，测试没问题。
+/// ★★★★★此处的测试用例都算是winform，只能同时运行一个测试用例
+/// </summary>
 namespace PerfLog.Core.Tests
 {
-    [TestClass()]
+    //因为程序的特殊性，同时只能有一个测试用例在跑，否则不准确或有问题。
+    [TestClass]
     public class PerfLogHelperTests
     {
-        [TestMethod()]
-        public void AttachTest()
+        static PerfLogHelperTests()
         {
-            Assert.Fail();
+            PerfLogHelper.Init();
+        }
+
+        public string GetContextStr()
+        {
+            return PerfLogHelper.GetContextItem().ToJson(isNeedFormat: true, isCanCyclicReference: false, ignoreNullValue: true);
+        }
+
+
+        [TestMethod]
+        public void Attach_Test_OnlyName()
+        {
+            PerfLogHelper.Clear();
+            PerfLogHelper.Attach("开始");
+            Thread.Sleep(1000);
+            PerfLogHelper.Attach("查酒店结束");
+
+            //调用具体接口
+            //拿到上下文
+            var children = @"[
+                                {
+                                'N': 'SearchLocalFare',
+                                'V': 123
+                                },
+                                {
+                                'N': 'SearchGDS',
+                                'V': 234
+                                },
+                                {
+                                'N': '合并',
+                                'V': 456
+                                },
+                                {
+                                'N': '总耗时',
+                                'V': 789
+                                }
+                            ]".FromJson<List<PerfLogNode>>();
+
+            //记录
+            var node = PerfLogHelper.Attach("查机票结束");
+            node.Children = children;
+
+            Thread.Sleep(100);
+            PerfLogHelper.Attach("查可选项结束");
+            var actual = GetContextStr();
+        }
+
+
+
+
+
+
+        [TestMethod]
+        public void Attach_Test_Name_BeginTime()
+        {
+            PerfLogHelper.Clear();
+            var timer = DateTime.Now.AddMilliseconds(-900000);
+            PerfLogHelper.Attach("开始", timer);
+            Thread.Sleep(1000);
+            PerfLogHelper.Attach("查酒店结束", timer);
+
+            Thread.Sleep(2000);
+            PerfLogHelper.Attach("查机票结束", timer);
+
+            Thread.Sleep(100);
+            PerfLogHelper.Attach("查可选项结束", timer);
+
+            timer = DateTime.Now;
+
+            Thread.Sleep(2000);
+            PerfLogHelper.Attach("1.查机票结束", timer);
+
+            Thread.Sleep(100);
+            PerfLogHelper.Attach("1.查可选项结束", timer);
+            var actual = GetContextStr();
+        }
+
+
+        [TestMethod]
+        public void Attach_Test_Name_Value()
+        {
+
+            PerfLogHelper.Clear();
+            var timer = DateTime.Now;
+
+            PerfLogHelper.Attach("开始", (DateTime.Now - timer).TotalMilliseconds);
+
+            Thread.Sleep(1000);
+            PerfLogHelper.Attach("查酒店结束", (DateTime.Now - timer).TotalMilliseconds);
+
+
+            Thread.Sleep(2000);
+            PerfLogHelper.Attach("查机票结束", (DateTime.Now - timer).TotalMilliseconds);
+
+            Thread.Sleep(100);
+            PerfLogHelper.Attach("查可选项结束", (DateTime.Now - timer).TotalMilliseconds);
+
+            var actual = GetContextStr();
+
+
+        }
+
+        /// <summary>
+        /// 有组，并发时日志很整齐
+        /// </summary>
+
+        [TestMethod]
+        public void Attach_Test_Parallel_Name_Group()
+        {
+            PerfLogHelper.Clear();
+            Parallel.Invoke(
+                () =>
+                {
+                    var timer = DateTime.Now;
+                    var guid = Guid.NewGuid();
+                    PerfLogHelper.Attach("组1.开始", guid);
+                    Thread.Sleep(1000);
+                    PerfLogHelper.Attach("组1.查酒店结束", guid);
+
+                    Thread.Sleep(2000);
+                    PerfLogHelper.Attach("组1.查机票结束", guid);
+
+                    Thread.Sleep(100);
+                    PerfLogHelper.Attach("组1.查可选项结束", guid);
+                    PerfLogHelper.Attach(guid);
+
+                },
+                () =>
+                {
+                    var timer = DateTime.Now;
+                    var guid = Guid.NewGuid();
+                    PerfLogHelper.Attach("组2.开始", guid);
+                    Thread.Sleep(1000);
+                    PerfLogHelper.Attach("组2.查酒店结束", guid);
+
+                    Thread.Sleep(2000);
+                    PerfLogHelper.Attach("组2.查机票结束", guid);
+
+                    Thread.Sleep(100);
+                    PerfLogHelper.Attach("组2.查可选项结束", guid);
+                    PerfLogHelper.Attach(guid);
+                }
+                );
+
+            var actual = GetContextStr();
+
+        }
+
+        /// <summary>
+        /// 并发时，没有组，日志很乱
+        /// </summary>
+        [TestMethod]
+        public void Attach_Test_Parallel_Name_NoGroup()
+        {
+            PerfLogHelper.Clear();
+            Parallel.Invoke(
+                () =>
+                {
+                    var timer = DateTime.Now;
+                    PerfLogHelper.Attach("组1.开始");
+                    Thread.Sleep(1000);
+                    PerfLogHelper.Attach("组1.查酒店结束");
+
+                    Thread.Sleep(2000);
+                    PerfLogHelper.Attach("组1.查机票结束");
+
+                    Thread.Sleep(100);
+                    PerfLogHelper.Attach("组1.查可选项结束");
+
+                },
+                () =>
+                {
+                    var timer = DateTime.Now;
+                    PerfLogHelper.Attach("组2.开始");
+                    Thread.Sleep(1000);
+                    PerfLogHelper.Attach("组2.查酒店结束");
+
+                    Thread.Sleep(2000);
+                    PerfLogHelper.Attach("组2.查机票结束");
+
+                    Thread.Sleep(100);
+                    PerfLogHelper.Attach("组2.查可选项结束");
+                }
+                );
+            var actual = GetContextStr();
+
         }
     }
+
 }
---
+++ PerfLog.Core/Helper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace PerfLog.Core.Extend
+{
+    public static class Helper
+    {
+        #region JSON序列化，反序列化 {1AC5559B-3615-4EA6-B15B-B6374B28D7A1}
+        /// <summary>
+        /// 使用json序列化为字符串
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="isNeedFormat">默认false</param>
+        /// <param name="isCanCyclicReference">默认true,生成的json每个对象会自动加上类似 "$id":"1","$ref": "1"</param>
+        /// <param name="dateTimeFormat">默认null,即使用json.net默认的序列化机制，如："\/Date(1439335800000+0800)\/"</param>
+        /// <param name="ignoreNullValue">值为null的不进行序列化（比如某个属性的值为null，序列化后没有此属性存在)</param>
+        /// <returns></returns>
+        public static string ToJson(this object input, bool isNeedFormat = false, bool isCanCyclicReference = true,
+                                    string dateTimeFormat = null, bool ignoreNullValue = true)
+        {
+            var settings = new JsonSerializerSettings();
+
+            if (ignoreNullValue)
+            {
+                settings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            }
+
+
+            if (isCanCyclicReference)
+            {
+                settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                settings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
+            }
+
+            if (!string.IsNullOrWhiteSpace(dateTimeFormat))
+            {
+                var jsonConverter = new List<JsonConverter>()
+                {
+                    new Newtonsoft.Json.Converters.IsoDateTimeConverter(){ DateTimeFormat = dateTimeFormat }//如： "yyyy-MM-dd HH:mm:ss"
+                };
+                settings.Converters = jsonConverter;
+            }
+
+            var format = isNeedFormat ? Formatting.Indented : Formatting.None;
+
+            var json = JsonConvert.SerializeObject(input, format, settings);
+            return json;
+        }
+
+        /// <summary>
+        /// 从序列化字符串里反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="dateTimeFormat">默认null,即使用json.net默认的序列化机制</param>
+        /// <returns></returns>
+        public static T FromJson<T>(this string input, string dateTimeFormat = null)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+            };
+
+
+            if (!string.IsNullOrWhiteSpace(dateTimeFormat))
+            {
+                var jsonConverter = new List<JsonConverter>()
+                {
+                    new Newtonsoft.Json.Converters.IsoDateTimeConverter(){ DateTimeFormat = dateTimeFormat }//如： "yyyy-MM-dd HH:mm:ss"
+                };
+                settings.Converters = jsonConverter;
+            }
+
+            return JsonConvert.DeserializeObject<T>(input, settings);
+        }
+
+        #endregion
+    }
+}
